75abd90467df888037e644fa108dd1c94180ec00
bhagesh01 <bansodebhageshbb08@gmail.com>
2024-12-14 16:35:33 +0530

    change for deploying

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import Menu from "@/components/shared/Menu";
 import DarkMode from "@/components/shared/DarkMode";
 import CurvedMenu from "@/components/shared/Menu";
 
